960ceb8b0e0065c58fc05691eeff338e1d0cb90a
modengann <99417989+modengann@users.noreply.github.com>
2022-09-07 10:40:39 -0400

    init

---
+++ src/Welcome.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Welcome {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        /* Write code that will:
+         * 1. Take input from the user
+         * 2. Decide if the number is divisible by 3
+         * 3. If it is divisible by 3, print out: "Divisible by 3!"
+         * 4. Otherwise, say: "Not divisible by 3!"
+         * 5. Run your code with a variety of inputs to make sure it works for all whole numbers
+         */
+        
+        
+    }
+}
